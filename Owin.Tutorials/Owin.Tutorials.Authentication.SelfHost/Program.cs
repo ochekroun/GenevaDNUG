e39bc259c608de26d3a55a37d6def6df3f4be0ef
Olivier Chekroun <ochekroun@infomaniak.ch>
2014-06-05 10:32:02 +0200

    Authentication middleware added.

---
+++ Owin.Tutorials/Owin.Tutorials.Authentication.SelfHost/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+
+namespace Owin.Tutorials.Authentication.SelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use(typeof(AuthenticationMiddleware));
+
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("DefaultWebApi", "{controller}/{id}", new { id = RouteParameter.Optional });
+
+            app.UseWebApi(config);
+
+        }
+    }
+
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse<string>("Hello, " + User.Identity.Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string baseUrl = "http://localhost:12345/";
+
+            using (WebApp.Start<Startup>(new StartOptions(baseUrl)))
+            {
+                Console.WriteLine("Press Enter to terminate.");
+                Console.Read();
+            }
+        }
+    }
+}
